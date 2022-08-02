51bc3a70e5ae9de1f3a939f6e4c0817ead130c95
Will <wmedders@gmail.com>
2022-08-02 17:46:58 -0400

    Tea has many customers through tea_customers

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -3,4 +3,7 @@ class Tea < ApplicationRecord
   validates :description, presence: true
   validates :temperature, numericality: true, presence: true
   validates :brew_time, presence: true
+
+  has_many :tea_customers
+  has_many :customers, through: :tea_customers
 end
