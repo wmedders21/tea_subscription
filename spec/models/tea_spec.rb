09b69228617a0f40be05cb74a88c11a3d9e0e099
Will <wmedders@gmail.com>
2022-08-02 16:55:08 -0400

    Tea Model validations

--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Tea, type: :model do
   describe 'validations' do
-    it should { validate_presence_of(:title) }
-    it should { validate_presence_of(:description) }
-    it should { validate_presence_of(:temperature) }
-    it should { validate_presence_of(:brew_time) }
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:temperature) }
+    it { should validate_numericality_of(:temperature) }
+    it { should validate_presence_of(:brew_time) }
   end
 end
